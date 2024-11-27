5f9d3835a07a270806f54375c656a2c99ea094aa
Eric <eric@ericw.tech>
2024-11-26 20:49:41 -0500

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,1 +1,30 @@
-
+def call(Map params) {
+	pipeline {
+		agent {
+			label 'docker'
+		}
+		stages {
+			stage ('Checkout') {
+				steps {
+				}
+			}
+			stage ('Build') {
+				steps {
+          configFileProvider([configFile(fileId: 'npmrc', targetLocation: '.npmrc')]) {
+            sh 'docker build --rm --secret id=npmrc,src=.npmrc -t ${params.name} .'
+          }
+				}
+			}
+			stage ('Clean') {
+				steps {
+          cleanWs()
+				}
+			}
+			stage ('Deploy') {
+				steps {
+          build params.deployJob
+				}
+			}
+		}
+	}
+}
