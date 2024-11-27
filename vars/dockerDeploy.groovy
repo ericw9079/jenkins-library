656ded18b3a1b170830e8cddf1d7aca051cba377
Eric <eric@ericw.tech>
2024-11-27 11:00:37 -0500

    Update dockerDeploy.groovy

--- vars/dockerDeploy.groovy
+++ vars/dockerDeploy.groovy
@@ -1,4 +1,19 @@
-def call(Map params) {
+def call(Map paramVars) {
+	def mounts = ''
+	def network = ''
+	if(paramVars.mounts) {
+		for(mount in paramVars.mounts) {
+			if(mount === 'logs') {
+				mounts += " --mount type=bind,source=${paramVars.projectRoot}/${mount},target=/${mount}"
+			} else {
+				mounts += " --mount type=bind,source=${paramVars.projectRoot}/${mount},target=/app/${mount}"
+			}
+		}
+	}
+	if(paramVars.networks){
+		network = " --network ${paramVars.networks.pop()}"
+	}
+	
 	pipeline {
 		agent {
 			label 'docker'
@@ -7,28 +22,37 @@ def call(Map params) {
 			stage ('Shutdown') {
 				steps {
 					catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
-						sh 'docker stop ${params.name}'
+						sh "docker stop ${paramVars.name}"
 					}
 				}
 			}
 			stage ('Remove Container') {
 				steps {
 					catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
-						sh 'docker stop ${params.name}'
+						sh "docker stop ${paramVars.name}"
 					}
 				}
 			}
 			stage ('Rotate Logs') {
 				steps {
+					dir(paramVars.projectRoot) {
+						catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
+							sh 'rotatelogs'
+						}
+					}
 				}
 			}
 			stage ('Setup') {
 				steps {
+					script {
+						sh "docker create --name ${paramVars.name} --restart always${mounts}${network} ${paramVars.imageName}"
+						
+					}
 				}
 			}
 			stage ('Start') {
 				steps {
-					sh 'docker start ${params.name}'
+					sh "docker start ${paramVars.name}"
 				}
 			}
 		}
