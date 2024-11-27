dc65dc8e3916584d3aaa9aba26be03d1e13f4de1
Eric <eric@ericw.tech>
2024-11-26 20:44:04 -0500

    Update dockerDeploy.groovy

--- vars/dockerDeploy.groovy
+++ vars/dockerDeploy.groovy
@@ -1,1 +1,36 @@
-
+def call(Map params) {
+	pipeline {
+		agent {
+			label 'docker'
+		}
+		stages {
+			stage ('Shutdown') {
+				steps {
+					catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
+						sh 'docker stop ${params.name}'
+					}
+				}
+				stage ('Remove Container') {
+					steps {
+						catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
+							sh 'docker stop ${params.name}'
+						}
+					}
+				}
+				stage ('Rotate Logs') {
+					steps {
+					}
+				}
+				stage ('Setup') {
+					steps {
+					}
+				}
+				stage ('Start') {
+            steps {
+                sh 'docker start ${params.name}'
+            }
+        }
+			}
+		}
+  }
+}
