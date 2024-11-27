a3363e5946e6fb8519fa5011a9a51678f420b5a2
Eric <eric@ericw.tech>
2024-11-27 14:38:15 -0500

    Update dockerDeploy.groovy

--- vars/dockerDeploy.groovy
+++ vars/dockerDeploy.groovy
@@ -1,3 +1,16 @@
+/**
+  This file defines the process for deploying docker images
+  Requirements:
+    - previously built docker image
+  Parameters (via Map variable):
+    - name (String): Name of docker container to deploy
+    - projectRoot (String): The root directory of the project (used for mounts and rotating the logs)
+    - imageName (String): Name of the image to launch the container from
+	- [OPTIONAL] ports (ArrayList<String>): List of ports to bind (no ports are bound by default)
+    - [OPTIONAL] networks (ArrayList<String>): List of networks to connect the container to (connects to bridge network by default per docker default)
+	- [OPTIONAL] mounts (ArrayList<String>): List of files/folders in the root directory to mount (nothing is mounted by default
+    - [OPTIONAL] networkAlias (String): DNS alias to use on additional container networks (the first connected network will not use the alias)
+ */
 def call(Map paramVars) {
 	def mounts = ''
 	def network = ''
